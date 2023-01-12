190f7bcc584420c2c072722d41090e57aca2976c
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-01-11 23:20:37 -0300

    feat: solving the convertion of a decimal to any base (2-16)

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"strings"
+)
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -9,6 +13,23 @@ package module01
 //   DecToBase(14, 16) => "E"
 //   DecToBase(14, 2) => "1110"
 //
+const hex = "0123456789ABCDEF"
+
 func DecToBase(dec, base int) string {
-	return ""
+	var sb strings.Builder
+	for dec > 0 {
+		sb.WriteByte(hex[dec%base])
+		dec = dec/base
+	}
+	return Reverse(sb.String())
+}
+
+func DecToBaseWithnamedReturn(dec, base int) (result string) {
+	var sb strings.Builder
+	for dec > 0 {
+		sb.WriteByte(hex[dec%base])
+		dec = dec/base
+	}
+	result = Reverse(sb.String())
+	return 
 }
